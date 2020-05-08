e87da772bf16a09976399ea0ae89fee186f0cafc
Tanishk <tanishkmahajan@gmail.com>
2020-05-08 17:39:15 +0530

    making all changes

--- js/script.js
+++ js/script.js
@@ -88,3 +88,71 @@ $(".dropdown-content").mouseout(function(){
 $(".skills").mouseout(function(){
     $(".dropdown-content").css("display", "none");
 });
+
+
+
+// -----------------------------------------------------------------
+
+$(document).ready(function () {
+  const isSmall = window.innerWidth < 500
+  $('.home-splash').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-splash .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
+  })
+
+  $('.home-about').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-about .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
+  })
+
+  $('.home-theme').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-theme .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
+  })
+
+  $('.home-contact').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-contact .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
+  })
+
+  $('body').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.events-list > .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
+  })
+})
+
